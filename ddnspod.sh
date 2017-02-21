7ddc518800e0a88a4252ec06daf07a886a4b41d3
JimReys <zsj1029@163.com>
2017-02-21 22:18:48 +0800

    Update ddnspod.sh

--- ddnspod.sh
+++ ddnspod.sh
@@ -1,144 +1,50 @@
-#!/bin/sh
+#!/bin/bash
+echo "dnspod"
 
-#################################################
-# AnripDdns v5.08
-# Dynamic DNS using DNSPod API
-# Original by anrip<mail@anrip.com>, http://www.anrip.com/ddnspod
-# Edited by ProfFan
-#################################################
 
-# OS Detection
-case $(uname) in
-  'Linux')
-    echo "Linux"
-    arIpAddress() {
-        local extip
-        extip=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1 | grep -Ev '(^127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^10\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.1[6-9]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.2[0-9]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.3[0-1]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^192\.168\.[0-9]{1,3}\.[0-9]{1,3}$)')
-        if [ "x${extip}" = "x" ]; then
-	        extip=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1 )
-        fi
-        echo $extip
-    }
-    ;;
-  'FreeBSD')
-    echo 'FreeBSD'
-    exit 100
-    ;;
-  'WindowsNT')
-    echo "Windows"
-    exit 100
-    ;;
-  'Darwin')
-    echo "Mac"
-    arIpAddress() {
-        ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}'
-    }
-    ;;
-  'SunOS')
-    echo 'Solaris'
-    exit 100
-    ;;
-  'AIX')
-    echo 'AIX'
-    exit 100
-    ;;
-  *) ;;
-esac
+#此脚本定时修改dnspod的IP
 
-# Get script dir
-# See: http://stackoverflow.com/a/29835459/4449544
-rreadlink() ( # Execute the function in a *subshell* to localize variables and the effect of `cd`.
+# 全局变量表
+arPass=arMail=""
 
-  target=$1 fname= targetDir= CDPATH=
-
-  # Try to make the execution environment as predictable as possible:
-  # All commands below are invoked via `command`, so we must make sure that `command`
-  # itself is not redefined as an alias or shell function.
-  # (Note that command is too inconsistent across shells, so we don't use it.)
-  # `command` is a *builtin* in bash, dash, ksh, zsh, and some platforms do not even have
-  # an external utility version of it (e.g, Ubuntu).
-  # `command` bypasses aliases and shell functions and also finds builtins 
-  # in bash, dash, and ksh. In zsh, option POSIX_BUILTINS must be turned on for that
-  # to happen.
-  { \unalias command; \unset -f command; } >/dev/null 2>&1
-  [ -n "$ZSH_VERSION" ] && options[POSIX_BUILTINS]=on # make zsh find *builtins* with `command` too.
-
-  while :; do # Resolve potential symlinks until the ultimate target is found.
-      [ -L "$target" ] || [ -e "$target" ] || { command printf '%s\n' "ERROR: '$target' does not exist." >&2; return 1; }
-      command cd "$(command dirname -- "$target")" # Change to target dir; necessary for correct resolution of target path.
-      fname=$(command basename -- "$target") # Extract filename.
-      [ "$fname" = '/' ] && fname='' # !! curiously, `basename /` returns '/'
-      if [ -L "$fname" ]; then
-        # Extract [next] target path, which may be defined
-        # *relative* to the symlink's own directory.
-        # Note: We parse `ls -l` output to find the symlink target
-        #       which is the only POSIX-compliant, albeit somewhat fragile, way.
-        target=$(command ls -l "$fname")
-        target=${target#* -> }
-        continue # Resolve [next] symlink target.
-      fi
-      break # Ultimate target reached.
-  done
-  targetDir=$(command pwd -P) # Get canonical dir. path
-  # Output the ultimate target's canonical path.
-  # Note that we manually resolve paths ending in /. and /.. to make sure we have a normalized path.
-  if [ "$fname" = '.' ]; then
-    command printf '%s\n' "${targetDir%/}"
-  elif  [ "$fname" = '..' ]; then
-    # Caveat: something like /var/.. will resolve to /private (assuming /var@ -> /private/var), i.e. the '..' is applied
-    # AFTER canonicalization.
-    command printf '%s\n' "$(command dirname -- "${targetDir}")"
-  else
-    command printf '%s\n' "${targetDir%/}/$fname"
-  fi
-)
-
-DIR=$(dirname -- "$(rreadlink "$0")")
-
-# Global Variables:
-
-# Token-based Authentication
-arToken=""
-# Account-based Authentication
-arMail=""
-arPass=""
-
-# Load config
-
-#. $DIR/dns.conf
-
-# Get Domain IP
-# arg: domain
-arNslookup() {
-    #wget --quiet --output-document=- $inter$1
-    echo ""
+# 获得外网地址
+arIpAddress() {
+    local extip
+    extip=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1 | grep -Ev '(^127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^10\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.1[6-9]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.2[0-9]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.3[0-1]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^192\.168\.[0-9]{1,3}\.[0-9]{1,3}$)')
+    if [ "x${extip}" = "x" ]; then
+        extip=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1 )
+    fi
+    echo $extip
 }
 
-# Get data
-# arg: type data
+# 查询域名地址
+# 参数: 待查询域名
+arNslookup() {
+    local dnsvr="114.114.114.114"
+    busybox nslookup ${1} $dnsvr | tr -d '\n[:blank:]' | sed 's/.\+1 [0-9\.]\+/\1/'
+}
+
+# 读取接口数据
+# 参数: 接口类型 待提交数据
 arApiPost() {
     local agent="AnripDdns/5.07(mail@anrip.com)"
     local inter="https://dnsapi.cn/${1:?'Info.Version'}"
-    if [ "x${arToken}" = "x" ]; then # undefine token
-        local param="login_email=${arMail}&login_password=${arPass}&format=json&${2}"
-    else
-        local param="login_token=${arToken}&format=json&${2}"
-    fi
+    local param="login_email=${arMail}&login_password=${arPass}&format=json&${2}"
     wget --quiet --no-check-certificate --output-document=- --user-agent=$agent --post-data $param $inter
 }
 
-# Update
-# arg: main domain  sub domain
+# 更新记录信息
+# 参数: 主域名 子域名
 arDdnsUpdate() {
     local domainID recordID recordRS recordCD myIP
     # Get domain ID
     domainID=$(arApiPost "Domain.Info" "domain=${1}")
     domainID=$(echo $domainID | sed 's/.*{"id":"\([0-9]*\)".*/\1/')
-    
+
     # Get Record ID
     recordID=$(arApiPost "Record.List" "domain_id=${domainID}&sub_domain=${2}")
     recordID=$(echo $recordID | sed 's/.*\[{"id":"\([0-9]*\)".*/\1/')
-    
+
     # Update IP
     myIP=$(arIpAddress)
     recordRS=$(arApiPost "Record.Ddns" "domain_id=${domainID}&record_id=${recordID}&sub_domain=${2}&record_type=A&value=${myIP}&record_line=默认")
@@ -153,11 +59,11 @@ arDdnsUpdate() {
     echo $recordRS | sed 's/.*,"message":"\([^"]*\)".*/\1/'
 }
 
-# DDNS Check
-# Arg: Main Sub
+# 动态检查更新
+# 参数: 主域名 子域名
 arDdnsCheck() {
     local postRS
-    local hostIP=$(arIpAddress)
+    local hostIP=$(arIpAdress)
     local lastIP=$(arNslookup "${2}.${1}")
     echo "hostIP: ${hostIP}"
     echo "lastIP: ${lastIP}"
@@ -171,11 +77,21 @@ arDdnsCheck() {
     return 1
 }
 
-# DDNS
-#echo ${#domains[@]}
-#for index in ${!domains[@]}; do
-#    echo "${domains[index]} ${subdomains[index]}"
-#    arDdnsCheck "${domains[index]}" "${subdomains[index]}"
-#done
+###################################################
 
-. $DIR/dns.conf
+# 设置用户参数 账号密码
+arMail="xxx@xxx.com"
+arPass="password"
+
+# 检查更新域名 一个参数不带www的域名，第二个是二级子域名
+arDdnsCheck "163.com" "mail"
+
+
+
+####################
+#
+# 最后加入 crontab
+# 0 2 * * * sh /tmp/ddnspod.sh > /tmp/ddnspod.log #更新域名解析地址 每天凌晨2点执行，并且输出日志
+#
+####################
+exit 0
